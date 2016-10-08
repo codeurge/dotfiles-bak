f7665406d20456fbec7caa42c6d7312edd3abae0
Derek Keith <derek@codeurge.com>
2016-10-08 02:47:10 -0400

    initial commit

---
+++ brew.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install ZSH.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install zsh
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
+  echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/zsh;
+fi;
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+brew install nodejs
+
+# Install more recent versions of some macOS tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install tmux
+
+# Install font tools.
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
+
+# Install other useful binaries.
+brew install ack
+brew install dark-mode
+brew install git
+brew install git-lfs
+brew install imagemagick --with-webp
+brew install lua
+brew install lynx
+brew install p7zip
+brew install pigz
+brew install pv
+brew install reattach-to-user-namespace
+brew install rename
+brew install speedtest_cli
+brew install ssh-copy-id
+brew install testssl
+brew install tree
+brew install webkit2png
+
+# Remove outdated versions from the cellar.
+brew cleanup
